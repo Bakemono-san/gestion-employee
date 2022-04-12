389cd6740abddf9af6453aeca44a745b0c13a782
Bakemono <82776147+OCHATOMIZU@users.noreply.github.com>
2022-04-12 14:55:58 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::resource('stocks', 'StockController');
 
 Route::get('/', [EmployeeController::class, 'index']);
